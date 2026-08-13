627fb4541da87b2182fda66af557a05dd6be63e2
sketchlibx <sketchlibofficial@gmail.com>
2026-08-13 12:52:32 +0530

    fix: class wrong name

--- app/src/main/java/neo/sketchware/ai/AiSettingsActivity.java
+++ app/src/main/java/neo/sketchware/ai/AiSettingsActivity.java
@@ -30,7 +30,7 @@ public class AiSettingsActivity extends BaseAppCompatActivity implements AiModel
     private AiModelAdapter adapter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ai_settings);
 
